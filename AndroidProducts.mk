6d2ad59bc820cb84ed7c5b7874c24f20ea89b476
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-01-25 18:30:01 +0100

    kaiser-os: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: blackshark Model: SHARK KSR-H0 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_kaiser-os.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_kaiser-os-user \
+    omni_kaiser-os-userdebug \
+    omni_kaiser-os-eng
